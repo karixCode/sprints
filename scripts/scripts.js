d3c86363d6fc0b5a70ecceeddbf290014fa741f8
karix <eivanov200606@mail.ru>
2024-11-11 13:22:29 +0700

    toggle language

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,6 @@
 const emoji = document.querySelectorAll('.emoji')
+const languageBlocks = document.querySelectorAll('.languages')
+const languageItems = document.querySelectorAll('.languages-item')
 
 emoji.forEach(emoji => {
     const imageSrc = emoji.getAttribute('src')
@@ -13,4 +15,17 @@ emoji.forEach(emoji => {
     emoji.addEventListener('mouseleave', () => replaceEmoji(imageSrc))
 
     window.innerWidth <= 768 ? replaceEmoji(gifSrc) : null
+})
+
+languageBlocks.forEach(languageBlock => {
+    languageBlock.addEventListener('click', (e) => {
+        const activeLanguagesId = e.target.dataset.languageId
+
+        if (!activeLanguagesId) return 0
+
+        languageItems.forEach(languageEl => languageEl.classList.remove('languages-active'))
+
+        const activeLanguages = document.querySelectorAll(`.languages-${activeLanguagesId}`)
+        activeLanguages.forEach(languageEl => languageEl.classList.add('languages-active'))
+    })
 })
